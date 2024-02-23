d524c1f1a76c5ba6eaa24303e8aef256decb4819
Carl aka Gnomeski <carlraysparks@gmail.com>
2024-02-23 18:17:29 +0000

    setup connection to db

--- env.py
+++ env.py
@@ -1,3 +1,4 @@
 import os
 
 os.environ.setdefault("SECRET_KEY", 'django-insecure-=1u=$o4%)5a$^63-mpvjd3o65*rt7doe56-!!nas0uws@s2+t')
+os.environ.setdefault("DATABASE_URL", "postgres://wmhcabsg:Z5ZLkM1E2jRNk3UB7_NRJtyt_lg2EfTU@trumpet.db.elephantsql.com/wmhcabsg")
