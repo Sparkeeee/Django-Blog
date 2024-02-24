9c9450366632102eb53ccf9c7c56c5d77325cbfe
Carl aka Gnomeski <carlraysparks@gmail.com>
2024-02-24 14:51:00 +0000

    add secure connection to db

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=1u=$o4%)5a$^63-mpvjd3o65*rt7doe56-!!nas0uws@s2+t)'
+SECRET_KEY = os.environ.get('Tanithoff1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
