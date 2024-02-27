96e9298ac79ae239da2a86b033a99dc09a5ae62e
Carl aka Gnomeski <carlraysparks@gmail.com>
2024-02-27 11:24:16 +0000

    update gitignore

--- env.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.environ.setdefault("SECRET_KEY", 'Tanithoff1')
-os.environ.setdefault("DATABASE_URL", 'postgres://wmhcabsg:Z5ZLkM1E2jRNk3UB7_NRJtyt_lg2EfTU@trumpet.db.elephantsql.com/wmhcabsg')
