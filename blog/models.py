133526ee2f417030e9d1f7c82ce9046cd940d219
Carl aka Gnomeski <carlraysparks@gmail.com>
2024-02-27 16:38:17 +0000

    add summernote to admin dashboard

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-STATUS = ((0, "Darft"), (1, "Published"))
+STATUS = ((0, "Draft"), (1, "Published"))
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
@@ -38,5 +38,5 @@ class Comment(models.Model):
     class Meta:
         ordering = ["created_on"]
     def __str__(self):
-        return f"Comment {self.body} | by {self.author}"
+        return f"Comment | {self.body} | by {self.author}"
 
