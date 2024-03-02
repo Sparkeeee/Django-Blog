4b78f33fbd1d2724558d40bd1ccd9cb803b28e4e
Carl aka Gnomeski <carlraysparks@gmail.com>
2024-03-02 18:21:52 +0000

    add comment editing

--- about/views.py
+++ about/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
+from .forms import CollaborateForm
 
 # Create your views here.
 def about_me(request):
     about = About.objects.order_by('updated_on').first()
+    if request.method == "POST":
+        print("Received a POST request")
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavor to respond within 2 working days.")
+
+    collaborate_form = CollaborateForm()
+
+    print("About to render template")
 
     return render(
         request,
         "about/about.html",
-        {'about': about},
+        {'about': about, "collaborate_form": collaborate_form},
     )
