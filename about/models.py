0e09e6f3b00c04d4171af52d3d912b0e3c6b5d7f
Carl aka Gnomeski <carlraysparks@gmail.com>
2024-03-04 16:28:49 +0000

    add cloudinary and image uploads

--- about/models.py
+++ about/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from cloudinary.models import CloudinaryField
 
 
 # Create your models here.
 class About(models.Model):
     title = models.CharField(max_length=200, unique=True)
+    featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     updated_on = models.DateTimeField(auto_now=True)
 
